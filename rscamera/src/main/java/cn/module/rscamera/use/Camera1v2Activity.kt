2513eed3d91f8764a5b2d8b6daca57d4239db980
dou <app_yoko@163.com>
2020-12-01 09:47:31 +0800

    update drawutil

--- rscamera/src/main/java/cn/module/rscamera/use/Camera1v2Activity.kt
+++ rscamera/src/main/java/cn/module/rscamera/use/Camera1v2Activity.kt
@@ -8,6 +8,7 @@ import cn.readsense.module.camera1.v2.CameraParams.PreviewCallback
 import cn.readsense.module.camera1.v2.CameraView
 import cn.readsense.module.util.DLog
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class Camera1v2Activity : BaseCoreActivity() {
 
@@ -31,11 +32,17 @@ class Camera1v2Activity : BaseCoreActivity() {
             object : PreviewCallback {
                 override fun onPreviewFrame(data: ByteArray, camera: Camera) {
                     //接受buffer数据，但是注意外部不要修改buffer
+
                     launch(Dispatchers.IO) {
                         DLog.d("launch ${Thread.currentThread().name} working ")
-                        val event = async { delay(20) }
-                        event.await()
-                        DLog.d("launch ${Thread.currentThread().name} end ")
+                        val cost = measureTimeMillis {
+                            val event1 = async { delay(2000) }
+                            val event2 = async { delay(2000) }
+                            event1.await()
+                            event2.await()
+                        }
+
+                        DLog.d("launch ${Thread.currentThread().name} end cost :$cost")
                     }
 
                 }
--- util/src/main/java/cn/readsense/module/util/DrawUtil.java
+++ util/src/main/java/cn/readsense/module/util/DrawUtil.java
@@ -7,17 +7,21 @@ import android.view.SurfaceView;
 
 public class DrawUtil {
 
-    public static void drawRect(SurfaceView outputView, float[] rect, Paint paint) {
+    public static void drawRect(SurfaceView outputView, float[][] rects, Paint paint) {
 
         Canvas canvas = outputView.getHolder().lockCanvas();
         if (canvas == null) return;
         canvas.drawColor(0, PorterDuff.Mode.CLEAR);
-        if (rect != null)
-            try {
-                canvas.drawRect(rect[0], rect[1], rect[2], rect[3], paint);
-            } catch (Exception e) {
-                e.printStackTrace();
+        if (rects != null) {
+            for (int i = 0; i < rects.length; i++) {
+                final float[] rect = rects[i];
+                try {
+                    canvas.drawRect(rect[0], rect[1], rect[2], rect[3], paint);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+        }
 
         outputView.getHolder().unlockCanvasAndPost(canvas);
     }
@@ -33,7 +37,7 @@ public class DrawUtil {
                 try {
                     paint.setStyle(Paint.Style.STROKE);
                     canvas.drawRect(rect[0], rect[1], rect[2], rect[3], paint);
-                    if (show[i] != null) {
+                    if (show != null && show[i] != null) {
                         paint.setStyle(Paint.Style.FILL);
                         canvas.drawText(show[i], rect[0], rect[1] - 30, paint);
                     }
